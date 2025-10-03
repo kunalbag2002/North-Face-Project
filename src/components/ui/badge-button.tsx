9c7bbf136821fbe9090872cc188ce64bbc6b68e6
kunalbag2002 <93586480+kunalbag2002@users.noreply.github.com>
2025-10-04 02:00:42 +0530

    Update badge-button.tsx

--- src/components/ui/badge-button.tsx
+++ src/components/ui/badge-button.tsx
@@ -7,17 +7,18 @@ interface BadgeButtonProps {
   children: React.ReactNode;
 }
 
-const BadgeButton: React.FC<BadgeButtonProps> = ({ children }) => {
+const BadgeButton: React.FC<BadgeButtonProps> = () => {
   return (
     <Badge
       variant="outline"
       className="mb-3 cursor-pointer rounded-[14px] border border-black/10 bg-white text-base md:left-6"
     >
       <IceCreamCone className=" fill-[#9ff01d] stroke-1 text-neutral-800" />{" "}
-      {children}
+      
     </Badge>
   )
 }
 
 export default BadgeButton
+
 
